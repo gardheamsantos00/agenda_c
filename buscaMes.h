bb3cdf67ec8509a3443bf096c07285d76fe43139
gardheamsantos00 <gardheam.santos@yahoo.com.br>
2017-11-29 22:16:14 -0200

    buscames

--- buscaMes.h
+++ buscaMes.h
@@ -1,13 +1,29 @@
-#include<stdlib.h>
-// recebe um vetor , o tamanho do vetor e mesprocurado, vai retornar
-// só se encontrou ou não, a impressão faz em outro lugar
-int buscaPorMes(int *vetor, int TAM,int mesProcurado){
+#include "pessoa.h"
+#include <stdio.h>
+#include <string.h>
+
+void buscaPorMes(int mes, Pessoa vet[]){
+    
     int i;
-    for(i = 0; i < TAM; i++){
-        if(mesProcurado == vetor[i].aniversario.mes){
-            //printf("%s", &vetor[i]);
-            return i; // mes encontrado
+
+    for(i=0;i<100;i++)
+    {
+        if(strcmp(vet[i].aniversario.mes,mes) == 0)
+        {
+            printf("Nome:%s\n",vet[i].nome);
+            printf("Email:%s\n",vet[i].email);
+            printf("País:%s         Estado:%s\n",vet[i].endereco.pais,vet[i].endereco.estado);
+            printf("Cidade:%s       Bairro:%s\n",vet[i].endereco.cidade,vet[i].endereco.bairro);
+            printf("Rua:%s          Numero:%d\n",vet[i].endereco.rua,vet[i].endereco.numero);
+            printf("CEP:%s\n",vet[i].endereco.cep);
+            printf("Complemento:%s\n",vet[i].endereco.complemento);
+            printf("Telefone:(%d)%d\n",vet[i].telefone.ddd,vet[i].telefone.numero);
+            printf("Aniversario:%d/%d/%d\n",vet[i].aniversario.dia,vet[i].aniversario.mes,vet[i].aniversario.ano);
+            printf("Observacoes:%s\n",vet[i].Observacao);
         }
-        return -1; // mes não encontrado
+
+        
+
     }
 }
+
