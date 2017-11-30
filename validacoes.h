27ee35275862d61046feb5f0cb51b9567aa119ce
gardheamsantos00 <gardheam.santos@yahoo.com.br>
2017-11-29 22:03:13 -0200

    ssssssss

--- validacoes.h
+++ validacoes.h
@@ -1,14 +1,27 @@
 #include<stdlib.h>
 
-void Valida_Email(){
+void Valida_Email(char *email){
+    int i=0,depoisArroba=0;
+    
+    if(strlen(email) < 8 ){
+        printf("digite o email no formato: nome@email.com\n");
+        printf("nome e meial com no mínimo 2 caracteres\n");
+        printf("email obrigatoriamente deve conter .COM \n");
+        return -1;
+    }
+    else {
+            
+
+
+        }
+    }
 
 }
-
 
 void Valida_Telefone(){
 
 }
 
 void Valida_Data(){
-    
+
 }
